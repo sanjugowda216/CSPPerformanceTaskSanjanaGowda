05dfbff645c00be4373f26442d5081fe2be9af88
sanjugowda216 <143921320+sanjugowda216@users.noreply.github.com>
2025-03-24 15:21:42 -0700

    basics

--- lemonade.py
+++ lemonade.py
@@ -1,12 +1,24 @@
 # The Purpose of my program is for users to order lemonade from a lemonade stand.
 # My program lets a user customize their lemonade order
 
-print("What a beautiful and sunny day! \nWelcomg to Sanjana's Lemonade Stand!\n-----------")
+print("What a beautiful and sunny day! \nWelcome to Sanjana's Lemonade Stand!\n------------------")
 name = input("What is your name: ")
 
-def order():
+def order(name):
     flavors = ['original','strawberry','peach','rasberry']
     for i in flavors:
         print(flavors.index(i)+1,i)
 
-order()
+    print("Hello "+name +"! What woould you like to order?") 
+    myFlavor = int(input("Please enter a number from 1-4: "))
+    if myFlavor == 1:
+        print("You ordered a "+flavors[0]+" lemonade!")
+    elif myFlavor == 2:
+        print("You ordered a "+flavors[1]+" lemonade!")
+    elif myFlavor == 3:
+        print("You ordered a "+flavors[2]+" lemonade!")
+    elif myFlavor == 4:
+        print("You ordered a "+flavors[3]+" lemonade!")
+    print("Have a great day "+name+"!!!")
+ 
+order(name)
