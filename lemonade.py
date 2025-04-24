c4f068bb6b68f6fc3e4eb3a5814004382876805d
sanjugowda216 <143921320+sanjugowda216@users.noreply.github.com>
2025-04-24 14:29:53 -0700

    while loop

--- lemonade.py
+++ lemonade.py
@@ -16,19 +16,17 @@ def order(name):
     myFlavor = input("Please enter a number from 1-4: ")
     
     #order response checking if input is a digit
-    if myFlavor.isdigit():
-        myFlavor = int(myFlavor)
-        if myFlavor == 1:
-            print("You ordered a "+flavors[0]+" lemonade!")
-        elif myFlavor == 2:
-            print("You ordered a "+flavors[1]+" lemonade!")
-        elif myFlavor == 3:
-            print("You ordered a "+flavors[2]+" lemonade!")
-        elif myFlavor == 4:
-            print("You ordered a "+flavors[3]+" lemonade!")
-        else:
-            print("Have a great day "+name+"!!!")           
-    else:
-        order(name)
+    while myFlavor not in ["1","2","3","4"]:
+        myFlavor = input("Invalid input. Please select a menu number from 1-4: ")
+        
+    if myFlavor == "1":
+        print("You ordered an "+flavors[0]+" lemonade!")
+    elif myFlavor == "2":
+        print("You ordered a "+flavors[1]+" lemonade!")
+    elif myFlavor == "3":
+        print("You ordered a "+flavors[2]+" lemonade!")
+    elif myFlavor == "4":
+        print("You ordered a "+flavors[3]+" lemonade!")
+
 #call to student developed procedure 
 order(name)
